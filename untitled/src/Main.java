60acdcac77509f6ce1769d47c811b3604deac516
Wojtanowicz <pawel.wojtanowicz@capgemini.com>
2023-03-03 12:34:23 +0100

    kuba i ola

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ola!");
+        System.out.println("Hello ola i kuba!");
         System.out.println("Hello new ola");
     }
 }
