03e6f57fe7e97e7b069aa99251c495d5b3428b47
Wojtanowicz <pawel.wojtanowicz@capgemini.com>
2023-03-03 12:10:14 +0100

    second second

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello Poland!");
-=======
         System.out.println("Hello world!");
         System.out.println("Hello new world");
->>>>>>> 51d834f (second commit second user)
     }
 }
